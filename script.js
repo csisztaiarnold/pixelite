ebdc4f5e9661bad1f99f5339048f405fee0c504b
Arnold Csisztai <csisztai.arnold@gmail.com>
2018-07-14 23:16:20 +0200

    Char choordinates, detecting unique colors in a char

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         '255,255,255',
         '103,55,43',
         '112,164,168',
-        '111,61,133',
+        '111,61,134',
         '88,141,67',
         '53,40,121',
         '184,199,111',
@@ -20,6 +20,7 @@ $(document).ready(function(){
         '108,94,181',
         '149,149,149'
     ];
+
     var canvas = document.getElementById("canvas");
     var previewCanvas = document.getElementById("previewCanvas");
     var ctx = canvas.getContext("2d");
@@ -39,14 +40,14 @@ $(document).ready(function(){
         changeCanvasSize();
     });
 
-    function changeCanvasSize() {
+    function changeCanvasSize(){
         $('#container').css('width', $(window).width() + 'px').css('height', $(window).height() + 'px');
     }
 
     // Color palette div
     var i;
     var paletteHtml = '';
-    for (i = 0; i < palette.length; ++i) {
+    for (i = 0; i < palette.length; ++i){
         paletteHtml += '<div class="color' + (i == 0 ? " active" : "" ) + '" data-rgb="' + palette[i] + '" style="background-color: rgba(' + palette[i] + ',1)"></div>';
     }
 
@@ -55,20 +56,20 @@ $(document).ready(function(){
     // Load an initial image into the background
     var img = new Image();
     img.src = 'img1.png';
-    img.onload = function () {
+    img.onload = function (){
         ctx.drawImage(img, 0, 0);
         refreshPreviewCanvas();
     };
 
     // Change the image on select
-    $('#options-container select').on('change', function() {
+    $('#options-container select').on('change', function(){
         img.src = this.value + '.png';
         ctx.drawImage(img, 0, 0);
         refreshPreviewCanvas();
     });
 
     // Clear the canvas
-    $('#clear').on('click', function() {
+    $('#clear').on('click', function(){
         ctx.save();
         ctx.setTransform(1, 0, 0, 1, 0, 0);
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -78,10 +79,6 @@ $(document).ready(function(){
         refreshPreviewCanvas();
     });
 
-    $('#canvas').on('hover', function() {
-
-    });
-
     // Mousevents
     $(canvas).bind({
         mousedown : function(){
@@ -89,9 +86,21 @@ $(document).ready(function(){
             drawPixel(event);
         },
         mousemove : function(){
-            var currentCoordinates = returnCurrentCoordinates(event);  
-            $('#coor-x').val(currentCoordinates[0]);
-            $('#coor-y').val(currentCoordinates[1]);            
+            var currentPixelCoordinates = returnCurrentPixelCoordinates(event);  
+            $('#coor-x').val(currentPixelCoordinates[0] + 1);
+            $('#coor-y').val(currentPixelCoordinates[1] + 1);
+
+            var currentCharCoordinates = returnCurrentCharCoordinates(event);  
+            $('#charcoor-x').val(currentCharCoordinates[0] + 1);
+            $('#charcoor-y').val(currentCharCoordinates[1] + 1);                      
+
+            var uniqueColors = returnUniqueColorsFromCurrentCharbyCharCoordinates(currentCharCoordinates);
+            if(uniqueColors.length == 1){     
+                uniqueColors[1] = uniqueColors[0]; // If there's only one color, the second color is the same   
+            }
+            $('#current-colors .color1').css('background-color', 'rgba(' + uniqueColors[0] + ')');
+            $('#current-colors .color2').css('background-color', 'rgba(' + uniqueColors[1] + ')');
+
             if(!down) return;
             drawPixel(event);
             refreshPreviewCanvas();
@@ -102,29 +111,74 @@ $(document).ready(function(){
     });
 
     // Update coordinates 
-    function returnCurrentCoordinates(event) {
-        var x = Math.floor((event.clientX + container.scrollLeft ) / zoomFactor);
-        var y = Math.floor((event.clientY + container.scrollTop ) / zoomFactor);
+    function returnCurrentPixelCoordinates(event){
+        var x = Math.floor((event.clientX + container.scrollLeft) / zoomFactor);
+        var y = Math.floor((event.clientY + container.scrollTop) / zoomFactor);
         return [x,y];
     }
 
+    // Update coordinates 
+    function returnCurrentCharCoordinates(event){
+        var x = Math.floor((event.clientX + container.scrollLeft) / zoomFactor / 8);
+        var y = Math.floor((event.clientY + container.scrollTop) / zoomFactor / 8);
+        return [x,y];
+    }
+
+    function returnUniqueColorsFromCurrentCharbyCharCoordinates(currentCharCoordinates){
+        // Get the up left x and y coordinate of the char
+        var charStartX = currentCharCoordinates[0] * 8;
+        var charStartY = currentCharCoordinates[1] * 8;
+        // Dump the chardata into an array
+        var imgData = ctx.getImageData(charStartX,charStartY,8,8);
+        var uniqueColors = colorsFromCharIntoArrayAndReturnUniques(imgData.data);
+        return uniqueColors;
+    }
+
     // Copying the main canvas into 
-    function refreshPreviewCanvas() {
+    function refreshPreviewCanvas(){
         var destCtx = previewCanvas.getContext('2d');
         destCtx.drawImage(canvas, 0, 0);    
     }
 
+    // Draw the actual pixel on the canvas
     function drawPixel(event) {
         // Calculate the position of the mouse
-        var currentCoordinates = returnCurrentCoordinates(event);  
-        createPixel(currentCoordinates[0], currentCoordinates[1], pixelColor);
+        var currentPixelCoordinates = returnCurrentPixelCoordinates(event);  
+        var currentCharCoordinates = returnCurrentCharCoordinates(event);  
+        var uniqueColors = returnUniqueColorsFromCurrentCharbyCharCoordinates(currentCharCoordinates);
+        // Todo: color replacement
+        if(uniqueColors.length < 2 || pixelColor + ',255' == uniqueColors[0] || pixelColor + ',255' == uniqueColors[1]) {
+            console.log('Cool, we can put colors here.');
+        } else {
+            console.log('No can do. Colorclash here.');
+        }
+        createPixel(currentPixelCoordinates[0], currentPixelCoordinates[1], pixelColor);
         refreshPreviewCanvas();
     }
 
-    function createPixel(pixelX, pixelY, color) {
+    // Create a pixel
+    function createPixel(pixelX, pixelY, color){
         ctx.putImageData( id, pixelX, pixelY );
         ctx.fillStyle = "rgba(" + color + ",1)";
         ctx.fillRect( pixelX, pixelY, 1, 1 );
+    }
+
+    // All colors from char into an array, then return the uniques
+    function colorsFromCharIntoArrayAndReturnUniques(data){
+        var i;
+        var n = 0;
+        var colorArray = [];
+        for(i = 0; i < data.length; i++){
+            if(i % 4 == 0) {
+                colorArray[n] = data[i] + ',' + data[i+1] + ',' + data[i+2] + ',' + data[i+3];
+                n++;
+            } 
+        }
+        uniqueItems = [];
+        $.each(colorArray, function(i, el){
+            if($.inArray(el, uniqueItems) === -1) uniqueItems.push(el);
+        });
+        return uniqueItems;
     }
 
     // Canvas Zoom
