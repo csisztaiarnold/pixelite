d484eb727ead80846d19dbe8ceb60c520549dd3e
Arnold Csisztai <csisztai.arnold@gmail.com>
2018-07-15 12:38:27 +0200

    V0.1
    - First considerable, actually useful release
    - Added dither brush support
    - Refactoring/code cleanup, commenting
    - Added warning message before closing tab

--- README.md
+++ README.md
@@ -1,23 +1,49 @@
-# Pixelite
+# Pixelite v0.1
 A Commodore 64 pixel editor made in HTML/JS
 
 Thanks for JackAsser for the idea ;)
 
-Currently just a very early proof of concept. Still a lot to do:
+Currently it ~~is just a very early proof of concept~~ could function as a full-fledged pixel editor for creating Commodore 64 hires images (320x200px, 2 colors per 8x8 char area). Still a lot to do!
 
-- grid
-- color clash handling
+- grid (character indicator overlay is functioning, though)
+- ~~hires~~ and multicolor color clash handling
 - undo
-- different graphics modes
+- preview window locator overlay
+- better zoom handling
+- ~~hires~~ and multicolor graphics modes
 - import/export
-- brushes
+- ~~ditherbrushes~~ possibility
+- change color under cursor
+- keyboard shortcuts
 - only tested on Chrome yet
 
 ## Help
 
 - Move main canvas with cursor buttons
-- Mouseweel on canvas: zoom in/out
 - Drag any window to move
 - Click on preview window to activate
 - Zoom in/out activated preview with mousewheel
 - Save with right click > Save As...
+
+## Running locally
+
+Since Chrome has some cross-origin issue if index.html is simply loaded into the browser, the best way to running the app locally is to run it in a server. For me, the easiest way was to install to run NodeJS's http-server and run it from the root of the project. So something like this, assuming NodeJS (and npm) is already installed:
+
+~~~
+npm install http-server -g
+cd /home/Projects/pixelate
+http-server ./
+~~~
+
+## Preview current state
+
+https://pixelite.idevele.com
+
+## History
+
+### v0.1
+
+- First considerable, actually useful release
+- Added dither brush support
+- Refactoring/code cleanup, commenting
+- Added warning message before closing tab
--- script.js
+++ script.js
@@ -1,9 +1,18 @@
 $(document).ready(function(){
+    
+    window.addEventListener("beforeunload", function (e) {
+        var confirmationMessage = "Are you sure you want to leave the page?";
+        e.returnValue = confirmationMessage;     // Gecko, Trident, Chrome 34+
+        return confirmationMessage;              // Gecko, WebKit, Chrome <34
+    });
 
+    // Draggable settings for the modal windows
     $("#preview-canvas-container, #palette-container, #options-container, #zoom").draggable({
         'handle': 'h1'
     });
 
+    // Basic good old Pepto palette from Project One
+    // TODO: add more palette configs
     var palette = [
         '0,0,0',
         '255,255,255',
@@ -23,18 +32,51 @@ $(document).ready(function(){
         '149,149,149'
     ];
 
-    var canvas = document.getElementById("canvas");
-    var previewCanvas = document.getElementById("previewCanvas");
-    var ctx = canvas.getContext("2d");
-    var zoomFactor = 8;
-    var previewZoomFactor = 1;
-    var previewZoomActive = false;
-    var id = ctx.createImageData(1,1);
-    var d = id.data;
-    var down = false;
-    var pixelColor = "0,0,0";
-    var hiresLimits = true;
+    var canvas = document.getElementById("canvas"); // Main canvas
+    var previewCanvas = document.getElementById("preview-canvas"); // Preview window canvas
+    var ctx = canvas.getContext("2d"); // The context
+    var zoomFactor = 32; // Initial zoom
+    var previewZoomFactor = 1; // Initial zoom of the preview window
+    var previewZoomActive = false; // This has no use at the moment. TODO: refactor
+    var id = ctx.createImageData(1,1); // Pixel size
+    var d = id.data; // Pixel data
+    var down = false; // Is mouse clicked?
+    var pixelColor = "0,0,0"; // Initial pixel color
+    var hiresLimits = true; // Are hires limitation turned on?
+    var ditherBrush = false;
+    // Only the following pixelindexes will show up during pixelling
+    // TODO: (for the far future) brush editor
+    var brush_1 = [
+            1,    3,    5,    7, 
+         8,   10,   12,   14, 
+           17,   19,   21,   23,
+        24,   26,   28,   30,
+           33,   35,   37,   39,
+        40,   42,   44,   46,
+           49,   51,   53,   55,
+        56,   58,   60,   62
+    ];
 
+    $('#brush_1').on('click', function(){
+        if($(this).hasClass('active')){
+            ditherBrush = false;
+            ditherBrushArray = [];
+            $(this).removeClass('active');
+        }else{
+            ditherBrush = true;
+            ditherBrushArray = brush_1;
+            $(this).addClass('active');
+        }
+    });
+
+    // Set the initial zoom to the main canvas
+    $('#canvas').css('zoom', zoomFactor); 
+
+    // Set the initial zoom to the main canvas
+    $('#preview-canvas').css('zoom', previewZoomFactor); 
+
+    // Are the hi-res limitation turned on?
+    // TODO: this will need refactoring once multicolor is introduced
     $("#hires-limit").on('change', function(){
         if($(this).is(':checked')){
             hiresLimits = true;
@@ -63,13 +105,12 @@ $(document).ready(function(){
         $('#container').css('width', $(window).width() + 'px').css('height', $(window).height() + 'px');
     }
 
-    // Color palette div
+    // Create the color palette div based on the current palette array
     var i;
     var paletteHtml = '';
     for (i = 0; i < palette.length; ++i){
         paletteHtml += '<div class="color' + (i == 0 ? " active" : "" ) + '" data-rgb="' + palette[i] + '" style="background-color: rgba(' + palette[i] + ',1)"></div>';
     }
-
     $('#palette').append(paletteHtml);
 
     // Load an initial image into the background
@@ -79,13 +120,6 @@ $(document).ready(function(){
         ctx.drawImage(img, 0, 0);
         refreshPreviewCanvas();
     };
-
-    // Change the image on select
-    $('#options-container select').on('change', function(){
-        img.src = this.value + '.png';
-        ctx.drawImage(img, 0, 0);
-        refreshPreviewCanvas();
-    });
 
     // Clear the canvas
     $('#clear').on('click', function(){
@@ -104,7 +138,7 @@ $(document).ready(function(){
             down = true;  
             drawPixel(event);
         },
-        mousemove : function(){
+        mousemove : function(){ 
             var currentPixelCoordinates = returnCurrentPixelCoordinates(event);  
             $('#coor-x').text(currentPixelCoordinates[0]);
             $('#coor-y').text(currentPixelCoordinates[1]);
@@ -132,14 +166,14 @@ $(document).ready(function(){
         }
     });
 
-    // Update coordinates 
+    // Update pixel coordinates 
     function returnCurrentPixelCoordinates(event){
         var x = Math.floor((event.clientX + container.scrollLeft) / zoomFactor);
         var y = Math.floor((event.clientY + container.scrollTop) / zoomFactor);
         return [x,y];
     }
 
-    // Update coordinates 
+    // Update char coordinates 
     function returnCurrentCharCoordinates(event){
         var x = Math.floor((event.clientX + container.scrollLeft) / zoomFactor / 8);
         var y = Math.floor((event.clientY + container.scrollTop) / zoomFactor / 8);
@@ -169,11 +203,16 @@ $(document).ready(function(){
         var currentPixelCoordinates = returnCurrentPixelCoordinates(event);  
         var currentCharCoordinates = returnCurrentCharCoordinates(event);  
         var uniqueColors = returnUniqueColorsFromCurrentCharbyCharCoordinates(currentCharCoordinates);
+        // Which pixel in order are we changing in the char (need it for the array index)?
         var pixelIndexX = currentPixelCoordinates[0] - (currentCharCoordinates[0] * 8);
         var pixelIndexY = currentPixelCoordinates[1] - (currentCharCoordinates[1] * 8);
         var pixelIndex = pixelIndexX + (pixelIndexY * 8);
-
+        // Hires limits apply if they're turned on, this will change eventually if I introduce multicolor graphics mode
         if(uniqueColors.length < 2 || pixelColor + ',255' == uniqueColors[0] || pixelColor + ',255' == uniqueColors[1] || hiresLimits == false) {
+            // If ditherbrush is turned on, put pixels only on the indexes which are defined in the ditherBrush array
+            if(ditherBrush == true && $.inArray(pixelIndex, ditherBrushArray) != -1) {
+                return false;
+            }
             createPixel(currentPixelCoordinates[0], currentPixelCoordinates[1], pixelColor);
         } else {
             colorChange(currentCharCoordinates, pixelIndex, pixelColor);
@@ -205,12 +244,14 @@ $(document).ready(function(){
                 n++;
             } 
         }
+        // Find and change the matching color in the array
         colorToChange = colorArray[pixelIndex];
         for(i = 0; i < colorArray.length; i++) {
             if(colorArray[i] == colorToChange){
                 colorArray[i] = pixelColor + ',255';
             }
         }
+        // Finally, reverse it back to the image data and put it back to the canvas
         var colorData = [];
         var n = 0;
         $.each(colorArray, function( index, value ) {
@@ -226,7 +267,7 @@ $(document).ready(function(){
         ctx.putImageData(imgData,charStartX,charStartY);
     }
 
-    // All colors from char into an array, then return the uniques
+    // Puts all colors from the actual char into an array, then return the uniques
     function colorsFromCharIntoArrayAndReturnUniques(data){
         var i;
         var n = 0;
@@ -244,7 +285,8 @@ $(document).ready(function(){
         return uniqueItems;
     }
 
-    // Canvas Zoom
+    // Canvas Zoom (scrollwheel zoom was removed till I think of a better way of handling it)
+    // TODO: keyboard shortcut, zooming proportionally from the center of the current viewport
     $('.zoom-option').on('click', function(){
         if($(this).hasClass('zoom-in')){
             zoomIndex = 4;
@@ -268,33 +310,34 @@ $(document).ready(function(){
         }
     });
 
-    // Activate-deactive preview window
-    $('#previewCanvas').on("click",function(){
+    // Activate-deactive preview window (this has no practical use currently)
+    // TODO: think of a better way to zoom the preview window (keyboard shortcut?)
+    $('#preview-canvas').on("click",function(){
         event.stopPropagation();
         previewZoomActive = true;
-        $('#previewCanvas').addClass('active');
+        $('#preview-canvas').addClass('active');
     });
 
     $(window).click(function() {
         previewZoomActive = false;
-        $('#previewCanvas').removeClass('active');
+        $('#preview-canvas').removeClass('active');
     });
 
     // Preview window zoom
-    $('#previewCanvas').bind('mousewheel', function(event){
+    $('#preview-canvas').bind('mousewheel', function(event){
         var wheel = event.originalEvent.wheelDelta;
         if (previewZoomFactor + wheel > 1) {
-            $('#previewCanvas').css('zoom', previewZoomFactor + 0.5);
+            $(this).css('zoom', previewZoomFactor + 0.5);
             previewZoomFactor = previewZoomFactor + 0.5;
             $('#preview-canvas-container h1 span em').text(previewZoomFactor);
         } else {
-            $('#previewCanvas').css('zoom', 1);
+            $(this).css('zoom', 1);
             previewZoomFactor = 1;
             $('#preview-canvas-container h1 span em').text(previewZoomFactor);
         }
     });
 
-
+    // Visual clue for the current color
     $('#palette .color').on('click', function(){
         pixelColor = $(this).data('rgb');
         $('#palette .color').removeClass('active');
